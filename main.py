d7c798dad7016e402d4b657ca9da930f3fb3297c
Abhinab Sukla <137639013+Abhinab-123@users.noreply.github.com>
2025-12-30 13:35:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,6 @@ st.caption("Represented By Abhi BABA👾")
 # 🔒 Backend-controlled personality
 DEFAULT_PERSONALITY = "Funny 😁"
 
-if st.button("Create Knowledgebase"):
-    create_vector_db()
-    st.success("Knowledgebase created successfully!")
-
 with st.form("qa_form"):
     question = st.text_input("Say whatever is on your mind 💭")
     submit = st.form_submit_button("Send")
